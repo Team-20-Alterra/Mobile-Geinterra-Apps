5551fc1d61c0f063dd2d9f6b1fd7df8abed6a3cc
mbahgojol <profghozimahdi@gmail.com>
2022-11-27 21:31:16 +0700

    fixing conflix

--- lib/ui/home/main_page.dart
+++ lib/ui/home/main_page.dart
@@ -5,6 +5,7 @@ import 'package:geinterra_apps/ui/home/home_page.dart';
 import '../utils/colors.dart';
 
 class MainPage extends StatefulWidget {
+
   const MainPage({Key? key}) : super(key: key);
 
   @override
