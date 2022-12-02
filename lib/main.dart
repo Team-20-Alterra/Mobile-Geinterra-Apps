a6ed4d469daccd34ce328c523b8b7c405bf49872
mbahgojol <profghozimahdi@gmail.com>
2022-12-02 19:18:47 +0700

    asdfasdf

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DetailInvoice(),
+      home: LoginPage(),
       onGenerateRoute: (settings) => configRouters(settings),
     );
   }
