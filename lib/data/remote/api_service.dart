8b4c0ea3b2ba8f1b17386aabc369679c1c5cb865
mbahgojol <profghozimahdi@gmail.com>
2022-12-09 21:01:07 +0700

    adjument add token

--- lib/data/remote/api_service.dart
+++ lib/data/remote/api_service.dart
@@ -11,7 +11,7 @@ class ApiService {
   final Dio _dio = Dio();
 
   ApiService() {
-    _dio.interceptors.add(CustomInterceptors(token: ""));
+    _dio.interceptors.add(CustomInterceptors(token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJjaG9zdGluZzYzQGdtYWlsLmNvbSIsInJvbGUiOiJBZG1pbiIsImV4cCI6MTY3MDg1MzMzN30.yuXKAwnKZ3tfdeXEskNd0aWJKeEy4oyzMDEZsiKOS1o"));
   }
 
   Future<ResponseInvoices> fetchInvoice() async {
--- lib/ui/utils/custom_interceptors.dart
+++ lib/ui/utils/custom_interceptors.dart
@@ -8,8 +8,7 @@ class CustomInterceptors extends LogInterceptor {
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    options.headers["Authorization"] =
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJjaG9zdGluZzYzQGdtYWlsLmNvbSIsInJvbGUiOiJBZG1pbiIsImV4cCI6MTY3MDg1MzMzN30.yuXKAwnKZ3tfdeXEskNd0aWJKeEy4oyzMDEZsiKOS1o";
+    options.headers["Authorization"] = token;
     super.onRequest(options, handler);
   }
 }
