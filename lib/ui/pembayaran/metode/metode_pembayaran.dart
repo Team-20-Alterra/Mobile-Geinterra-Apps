952ea2497f4bc3ef71a84d810482b82fbc6acb60
HandivaPutri/flutter_rayi-handiva-putri <https://github.com/HandivaPutri/flutter_rayi-handiva-putri.git>
2022-12-14 15:08:39 +0700

    abc

--- lib/ui/pembayaran/metode/metode_pembayaran.dart
+++ lib/ui/pembayaran/metode/metode_pembayaran.dart
@@ -16,6 +16,20 @@ class MetodePembayaran extends StatefulWidget {
 
 class _MetodePembayaranState extends State<MetodePembayaran> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    //   myAppBar(
+    //       title: 'Metode Pembayaran',
+    //       back: () {
+    //         Navigator.pop(context);
+    //       });
+    // });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: myAppBar(
@@ -23,130 +37,131 @@ class _MetodePembayaranState extends State<MetodePembayaran> {
           back: () {
             Navigator.pop(context);
           }),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Metode Pembayaran',
-              style: heading6.copyWith(color: textBlack),
-            ),
-            const SizedBox(height: 16.0),
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, MetodeBank.routeName);
-              },
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: primaryGrey,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(18.0),
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        'Rekening Bank',
-                        style: regular14pt.copyWith(color: textBlack),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Icon(
-                        Icons.chevron_right_rounded,
-                        size: 24.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, MetodeVA.routeName);
-              },
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: primaryGrey,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(18.0),
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Text(
-                        'Virtual Akun',
-                        style: regular14pt.copyWith(color: textBlack),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: Icon(
-                        Icons.chevron_right_rounded,
-                        size: 24.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: primaryGrey,
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(18.0),
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Text(
-                      'Apakah kamu mempunyai masalah pembayaran?',
-                      style: regular14pt.copyWith(color: textBlack),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Bantuan()));
-                      },
-                      child: Text(
-                        'Bantuan',
-                        style: bold.copyWith(color: textBlue),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: SizedBox(height: 23),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(16.0),
+      //   child: Column(
+      //     crossAxisAlignment: CrossAxisAlignment.start,
+      //     children: [
+      //       Text(
+      //         'Metode Pembayaran',
+      //         style: heading6.copyWith(color: textBlack),
+      //       ),
+      //       const SizedBox(height: 16.0),
+      //       InkWell(
+      //         onTap: () {
+      //           Navigator.pushNamed(context, MetodeBank.routeName);
+      //         },
+      //         child: Container(
+      //           width: double.infinity,
+      //           decoration: BoxDecoration(
+      //             color: Colors.white,
+      //             border: Border.all(
+      //               color: primaryGrey,
+      //             ),
+      //             borderRadius: BorderRadius.all(
+      //               Radius.circular(18.0),
+      //             ),
+      //           ),
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //             children: [
+      //               Padding(
+      //                 padding: const EdgeInsets.all(16.0),
+      //                 child: Text(
+      //                   'Rekening Bank',
+      //                   style: regular14pt.copyWith(color: textBlack),
+      //                 ),
+      //               ),
+      //               Padding(
+      //                 padding: EdgeInsets.all(16.0),
+      //                 child: Icon(
+      //                   Icons.chevron_right_rounded,
+      //                   size: 24.0,
+      //                 ),
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //       ),
+      //       SizedBox(height: 16.0),
+      //       InkWell(
+      //         onTap: () {
+      //           Navigator.pushNamed(context, MetodeVA.routeName);
+      //         },
+      //         child: Container(
+      //           width: double.infinity,
+      //           decoration: BoxDecoration(
+      //             color: Colors.white,
+      //             border: Border.all(
+      //               color: primaryGrey,
+      //             ),
+      //             borderRadius: BorderRadius.all(
+      //               Radius.circular(18.0),
+      //             ),
+      //           ),
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //             children: [
+      //               Padding(
+      //                 padding: EdgeInsets.all(16.0),
+      //                 child: Text(
+      //                   'Virtual Akun',
+      //                   style: regular14pt.copyWith(color: textBlack),
+      //                 ),
+      //               ),
+      //               Padding(
+      //                 padding: EdgeInsets.all(16.0),
+      //                 child: Icon(
+      //                   Icons.chevron_right_rounded,
+      //                   size: 24.0,
+      //                 ),
+      //               ),
+      //             ],
+      //           ),
+      //         ),
+      //       ),
+      //       SizedBox(height: 16.0),
+      //       Container(
+      //         width: double.infinity,
+      //         decoration: BoxDecoration(
+      //           color: Colors.white,
+      //           border: Border.all(
+      //             color: primaryGrey,
+      //           ),
+      //           borderRadius: BorderRadius.all(
+      //             Radius.circular(18.0),
+      //           ),
+      //         ),
+      //         child: Column(
+      //           crossAxisAlignment: CrossAxisAlignment.start,
+      //           children: [
+      //             Padding(
+      //               padding: EdgeInsets.all(16.0),
+      //               child: Text(
+      //                 'Apakah kamu mempunyai masalah pembayaran?',
+      //                 style: regular14pt.copyWith(color: textBlack),
+      //               ),
+      //             ),
+      //             Padding(
+      //               padding: EdgeInsets.all(16.0),
+      //               child: TextButton(
+      //                 onPressed: () {
+      //                   Navigator.push(context,
+      //                       MaterialPageRoute(builder: (context) => Bantuan()));
+      //                 },
+      //                 child: Text(
+      //                   'Bantuan',
+      //                   style: bold.copyWith(color: textBlue),
+      //                 ),
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
