98cde6a16a856688272314e98a0ab72259311826
mbahgojol <profghozimahdi@gmail.com>
2022-12-14 15:03:33 +0700

    kokoko

--- lib/data/remote/api_service.dart
+++ lib/data/remote/api_service.dart
@@ -4,20 +4,26 @@ import 'package:dio/dio.dart';
 import 'package:geinterra_apps/data/model/Response_invoices.dart';
 import 'package:geinterra_apps/data/model/register_model.dart';
 
-import '../../ui/utils/custom_interceptors.dart';
-
 class ApiService {
   final String _baseUrl =
       "http://ec2-18-181-241-210.ap-northeast-1.compute.amazonaws.com:8000/api/v1/";
   final Dio _dio = Dio();
 
   ApiService() {
-    _dio.interceptors.add(CustomInterceptors(token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImVtYWlsIjoiY2hvc3Rpbmc2M0BnbWFpbC5jb20iLCJyb2xlIjoiQWRtaW4iLCJleHAiOjE2NzExMDU1MTV9.D7N8yDHCPLZ1WCCN_10tvwG2CyJX1hmjZqfryhhLoHU"));
+    _dio.interceptors.add(LogInterceptor());
   }
 
   Future<ResponseInvoices> fetchInvoice() async {
     try {
-      var response = await _dio.get("${_baseUrl}invoices");
+      var response = await _dio.get(
+        "${_baseUrl}invoices",
+        options: Options(
+          headers: {
+            "Authorization":
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAsImVtYWlsIjoiY2hvc3Rpbmc2M0BnbWFpbC5jb20iLCJyb2xlIjoiQWRtaW4iLCJleHAiOjE2NzExMDU1MTV9.D7N8yDHCPLZ1WCCN_10tvwG2CyJX1hmjZqfryhhLoHU",
+          },
+        ),
+      );
 
       return ResponseInvoices.fromJson(response.data);
     } on DioError catch (ex) {
